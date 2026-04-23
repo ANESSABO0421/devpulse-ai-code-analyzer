72de8e8c07c84d627bd6ac6b3ebcb389b643fe91
Anees Aboobacker <aneesaboo123@gmail.com>
2026-04-23 21:38:25 +0530

    fix: remove .next from git tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RjeHhKnZuCfarkIhZgZJTs7pPY87toNexNrUzlz5UEk=\"\n}"
--- .next/dev/static/chunks/components_SoftAurora_tsx_10h.u~y._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_ogl_src_10~k10_._.js","static/chunks/components_SoftAurora_tsx_0nrq.xz._.js"],
-    source: "dynamic"
-});
